2d5988523d1316d504e1d337725727865f4a89dd
Tommy TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-02-23 19:53:28 +0000

    Next en mode Server / SSR

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,21 +1,32 @@
-"use client";
-import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link";
-import Form from "./Form";
-console.log("about");
+
 export default function About() {
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<p>
 					Get started by editing&nbsp;
-					<Link href="/" className={styles.code}>
+					<a href="/" className={styles.code}>
 						Homepage
-					</Link>
+					</a>
 				</p>
-				<div>ici</div>
+				<div>
+					<a
+						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer">
+						By{" "}
+						<Image
+							src="/vercel.svg"
+							alt="Vercel Logo"
+							className={styles.vercelLogo}
+							width={100}
+							height={24}
+							priority
+						/>
+					</a>
+				</div>
 			</div>
 
 			<div className={styles.center}>
@@ -30,7 +41,51 @@ export default function About() {
 			</div>
 
 			<div className={styles.grid}>
-				<Form /> {/* Utilisation du composant de formulaire ici */}
+				<a
+					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer">
+					<h2>
+						Docs <span>-&gt;</span>
+					</h2>
+					<p>Find in-depth information about Next.js features and API.</p>
+				</a>
+
+				<a
+					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer">
+					<h2>
+						Learn <span>-&gt;</span>
+					</h2>
+					<p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+				</a>
+
+				<a
+					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer">
+					<h2>
+						Templates <span>-&gt;</span>
+					</h2>
+					<p>Explore starter templates for Next.js.</p>
+				</a>
+
+				<a
+					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer">
+					<h2>
+						Deploy <span>-&gt;</span>
+					</h2>
+					<p>
+						Instantly deploy your Next.js site to a shareable URL with Vercel.
+					</p>
+				</a>
 			</div>
 		</main>
 	);
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link";
-console.log("home");
+
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<p>
 					Get started by editing&nbsp;
-					<Link href="/about" className={styles.code}>
+					<a href="/about" className={styles.code}>
 						About page
-					</Link>
+					</a>
 				</p>
 				<div></div>
 			</div>
 
-			<div className={styles.center}>
-				<p>Get started by editing</p>
-			</div>
+			<div className={styles.center}></div>
 
-			<div className={styles.grid}>
-				<p>Get started by editing</p>
-			</div>
+			<div className={styles.grid}></div>
 		</main>
 	);
 }
